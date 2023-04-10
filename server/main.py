1940d818d012cf142a93735419554ac25e89238b
Grokgpt <grok@grokgpt.com>
2023-04-10 16:01:33 -0500

    Changed port again.

--- server/main.py
+++ server/main.py
@@ -163,4 +163,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8090, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=8080, reload=True)
